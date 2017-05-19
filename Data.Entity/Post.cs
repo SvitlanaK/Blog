190e55f4e94b2d84ed4fe932b63056a08daf39dc
Svitlana_Karapugina <sw.karapugina@gmail.com>
2017-05-20 02:48:29 +0300

    New Blog Mvc App

---
+++ Data.Entity/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data.Entity
+{
+	public class Post
+	{
+		public int Id { get; set; }
+		public string AuthorName { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		[Required(ErrorMessage = "Enter the Issued date.")]
+		[DataType(DataType.Date)]
+		public DateTime Date { get; set; }
+		public byte[] Image { get; set; }
+	}
+}
---
+++ WebApplication1/Controllers/AdminController.cs
@@ -0,0 +1,115 @@
+﻿using Data.Entity;
+using Data.Entity.Context;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+	public class AdminController : Controller
+	{
+		private EFDbContext db = new EFDbContext();
+
+		public ActionResult Dashboard()
+		{
+			return View();
+		}
+		public ActionResult EditeFile()
+		{
+			return View();
+		}
+		public ActionResult Posts_Read([DataSourceRequest]DataSourceRequest request)
+		{
+			IQueryable<Post> posts = db.Posts;
+			DataSourceResult result = posts.ToDataSourceResult(request, post => new {
+				Id = post.Id,
+				AuthorName = post.AuthorName,
+				Title = post.Title,
+				Description = post.Description,
+				Date = post.Date,
+				Image = post.Image
+			});
+
+			return Json(result);
+		}
+
+		[AcceptVerbs(HttpVerbs.Post)]
+		public ActionResult Posts_Create([DataSourceRequest]DataSourceRequest request, Post post)
+		{
+			if (ModelState.IsValid)
+			{
+				var entity = new Post
+				{
+					AuthorName = post.AuthorName,
+					Title = post.Title,
+					Description = post.Description,
+					Date = post.Date,
+					Image = post.Image
+				};
+
+				db.Posts.Add(entity);
+				db.SaveChanges();
+				post.Id = entity.Id;
+			}
+
+			return Json(new[] { post }.ToDataSourceResult(request, ModelState));
+		}
+
+		[AcceptVerbs(HttpVerbs.Post)]
+		public ActionResult Posts_Update([DataSourceRequest]DataSourceRequest request, Post post)
+		{
+			if (ModelState.IsValid)
+			{
+				var entity = new Post
+				{
+					Id = post.Id,
+					AuthorName = post.AuthorName,
+					Title = post.Title,
+					Description = post.Description,
+					Date = post.Date,
+					Image = post.Image
+				};
+
+				db.Posts.Attach(entity);
+				db.Entry(entity).State = EntityState.Modified;
+				db.SaveChanges();
+			}
+
+			return Json(new[] { post }.ToDataSourceResult(request, ModelState));
+		}
+
+		[AcceptVerbs(HttpVerbs.Post)]
+		public ActionResult Posts_Destroy([DataSourceRequest]DataSourceRequest request, Post post)
+		{
+			if (ModelState.IsValid)
+			{
+				var entity = new Post
+				{
+					Id = post.Id,
+					AuthorName = post.AuthorName,
+					Title = post.Title,
+					Description = post.Description,
+					Date = post.Date,
+					Image = post.Image
+				};
+
+				db.Posts.Attach(entity);
+				db.Posts.Remove(entity);
+				db.SaveChanges();
+			}
+
+			return Json(new[] { post }.ToDataSourceResult(request, ModelState));
+		}
+
+		//protected override void Dispose(bool disposing)
+		//{
+		//	db.Dispose();
+		//	base.Dispose(disposing);
+		//}
+	}
+}
